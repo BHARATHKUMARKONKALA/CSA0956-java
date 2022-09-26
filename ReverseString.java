afcf7a411b627f12521cb028a68306256c3130ec
BHARATHKUMARKONKALA <112711975+BHARATHKUMARKONKALA@users.noreply.github.com>
2022-09-26 08:58:54 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,9 @@
+public class ReverseString {
+    public static void main(string[] args) {
+        string s1 = "Hello world";
+        for(int i=s1.length()-1;i>=0;i=--)
+        {
+            system.out.print(s1.charAt(i));
+        }
+    }
+}
